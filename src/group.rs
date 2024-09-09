a41b51827c338a0bcee3d33b28abfc6c8736aee7
unmummied <unmummied@gmail.com>
2024-09-10 07:46:41 +0900

    add decomposition

--- src/group.rs
+++ src/group.rs
@@ -4,6 +4,8 @@ use crate::prime::*;
 pub trait Group {
     fn order(&self) -> Result<Num, String>;
     fn cyclic_group(&self, generator: Point) -> Result<Vec<Point>, String>;
+    fn solutions(&self) -> Result<Vec<Point>, String>;
+    fn decomposition(&self) -> Result<(Num, Num), String>;
 }
 
 impl Group for EllipticCurve {
@@ -30,6 +32,27 @@ impl Group for EllipticCurve {
         }
         Ok(cycle)
     }
+    fn solutions(&self) -> Result<Vec<Point>, String> {
+        let mut points = vec![Point::Inf];
+        for x in 0..self.prime {
+            for y in 0..self.prime {
+                if self.lhs(y) == self.rhs(x) {
+                    points.push(Point::Affine(x, y));
+                }
+            }
+        }
+        Ok(points)
+    }
+    fn decomposition(&self) -> Result<(Num, Num), String> {
+        let mut max_len = 0;
+        for sol in self.solutions()? {
+            let len = self.cyclic_group(sol)?.len();
+            if len > max_len {
+                max_len = len;
+            }
+        }
+        Ok((self.order()? / max_len as Num, max_len as Num))
+    }
 }
 
 #[cfg(test)]
@@ -53,6 +76,22 @@ mod test {
             let curve = EllipticCurve::new(3, 11, 53).unwrap();
             assert_eq!(57, curve.cyclic_group(Point::Affine(9, 5)).unwrap().len());
             assert_eq!(19, curve.cyclic_group(Point::Affine(38, 47)).unwrap().len());
+        }
+        #[test]
+        fn test_solutions() {
+            let prime = 53;
+            for a in 0..prime {
+                for b in 0..prime {
+                    if a == 0 && b == 0 {
+                        continue;
+                    }
+                    let curve = EllipticCurve::new(a, b, prime).unwrap();
+                    assert_eq!(
+                        curve.order().unwrap(),
+                        curve.solutions().unwrap().len() as Num
+                    );
+                }
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,32 @@ mod group;
 mod prime;
 use curve::*;
 use group::*;
+use prime::Prime;
 
 fn main() -> Result<(), String> {
-    let prime = 5;
-    let curve = EllipticCurve::new(1, 1, prime)?;
+    let prime = 47;
+    let curve = EllipticCurve::new(-1, 0, prime)?;
     println!("Elliptic curve: {}", curve);
 
-    let gene = curve.represent(Point::Affine(73, 24))?;
+    let gene = curve.represent(Point::Affine(0, 0))?;
     let cycle = curve.cyclic_group(gene)?;
     println!("order of curve: {}", curve.order().unwrap());
     println!("             g: {}", gene);
     println!("    order of g: {}", cycle.len());
     for (i, pt) in cycle.iter().enumerate() {
         println!("{:>2}: {}", i, pt);
+    }
+
+    for p in 2..100 {
+        if p.is_prime()? {
+            let curve = EllipticCurve::new(-1, 0, p).unwrap();
+            println!(
+                "{:>2}, {:>2}, {:?}",
+                p,
+                curve.order()?,
+                curve.decomposition()?
+            );
+        }
     }
 
     Ok(())
